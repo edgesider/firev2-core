ce23da1b241c23afaee990029f4e89db48d7520e
ykai <yingkaidang@gmail.com>
2020-02-02 15:30:59 +0800

    Monitor: add logger; remove parent log pipes and sync pipe.

--- firev2/v2ray.py
+++ firev2/v2ray.py
@@ -7,6 +7,7 @@ import signal
 import socket
 import subprocess
 import sys
+import logging
 from io import FileIO
 from multiprocessing import Process
 from typing import IO, Dict, List, Tuple, Optional
@@ -20,13 +21,10 @@ class V2rayManager:
 
     def start(self, config: Dict):
         m = Monitor(config=config)
-        self._v2_log_file = list(m.create_v2_log_pipe())
         m.start()
 
     def stop(self):
-        """stop running monitor
-        Note:
-        """
+        """stop running monitor"""
         for fp in self._v2_log_file:
             if fp:
                 fp.close()
@@ -76,31 +74,29 @@ class Monitor(Process):
            provide a way for it to get these log. The solution is
            listening on some unix domain sockets and write logs to
            the sockets connecting to them;
-        TODO add log
     """
+    _logger: Optional[logging.Logger]
     _config: dict
     _v2: Optional[subprocess.Popen]
     _v2_access_log_reader: Optional[IO]
     _v2_error_log_reader: Optional[IO]
     _v2_config_filename: str
-    _parent_log_pipe: Tuple[Tuple[Optional[FileIO], Optional[FileIO]],
-                            Tuple[Optional[FileIO], Optional[FileIO]]]
     _sock_access_fname: str
     _sock_error_fname: str
     _sock_access: Optional[socket.socket]
     _sock_error: Optional[socket.socket]
     _redirect_access_files: List[IO]
     _redirect_error_files: List[IO]
-    _data_to_write: Dict[IO, bytes]
+    _redirect_data_to_write: Dict[IO, bytes]
 
     def __init__(self, config: dict):
         super().__init__()
+        self._logger = None
         self._config = config
         self._v2 = None
         self._v2_access_log_reader = None
         self._v2_error_log_reader = None
         self._v2_config_filename = ''
-        self._parent_log_pipe = ((None, None), (None, None))
 
         self._sock_access_fname = self.get_v2_access_log_filename()
         self._sock_error_fname = self.get_v2_error_log_filename()
@@ -109,57 +105,25 @@ class Monitor(Process):
 
         self._redirect_access_files = []
         self._redirect_error_files = []
-        self._data_to_write = {}
+        self._redirect_data_to_write = {}
 
         self._should_exit = False
         self._child_exited = False
         self._should_restart = False
 
-        self._sync_pipe = os.pipe()
-
         if not os.path.exists(self._get_work_dir()):
             os.mkdir(self._get_work_dir())
 
-    def start(self):
-        super().start()
-        # after sub-process started, we need close the pipe in parent.
-        os.close(self._sync_pipe[1])
-        for fp in self._parent_log_pipe[1]:
-            fp.close()
-        d = os.read(self._sync_pipe[0], 1)
-        assert d == b'@', 'monitor start failed'
-        os.close(self._sync_pipe[0])
-
     def run(self):
         assert not self.is_running(), 'another monitor is already running'
-        self._logfile_as_stdout_err()
-        self._daemonize()
         try:
-            """
-            Steps:
-                1. Write pid file
-                2. Open unix domain sockets
-                3. Install handlers for SIGCHLD and SIGTERM
-                4. Start v2ray
-                5. Poll file descriptors
-            """
-            self._write_pid_file()
-            self._listen_log_sockets()
-            self._install_signal_handler()
-            assert os.write(self._sync_pipe[1], b'@') == 1, 'monitor start failed'
-            self._open_v2_log_files()
-            while True:
-                self._start_v2ray()
-                self._poll()
-                # if self._poll returns, it means we need to restart v2ray
-                self.release_v2_resources()
-                for file, d in self._data_to_write.items():
-                    self._data_to_write[file] += b'v2ray restarting...\n'
+            self._run()
         finally:
-            try:
-                self._v2.kill()
-            except Exception:
-                pass
+            if self._v2:
+                try:
+                    self._release_v2_resources()
+                except Exception:
+                    pass
             # remove pid file and socket files if any exception occurred
             for f in [self.get_pid_filename(),
                       self._sock_access_fname,
@@ -168,18 +132,48 @@ class Monitor(Process):
                 if os.path.exists(f):
                     os.remove(f)
 
-    def release_v2_resources(self):
+    def _run(self):
+        self._create_logger()
+        self._daemonize()
+        """
+        Steps:
+        1. Write pid file
+        2. Open unix domain sockets
+        3. Install handlers for SIGCHLD and SIGTERM
+        4. Start v2ray
+        5. Poll file descriptors
+        """
+        self._write_pid_file()
+        self._listen_log_sockets()
+        self._install_signal_handler()
+        self._open_v2_log_files()
+        while True:
+            self._start_v2ray()
+            self._poll()
+            # if self._poll returns, it means we need to restart v2ray
+            self._release_v2_resources()
+            for file, d in self._redirect_data_to_write.items():
+                self._redirect_data_to_write[file] += b'[monitor] v2ray restarting...\n'
+
+    def _release_v2_resources(self):
         prev = signal.signal(signal.SIGCHLD, signal.SIG_BLOCK)
         self._v2.terminate()
-        self._v2.wait()
-        self._v2_access_log_reader.close()
-        self._v2_error_log_reader.close()
+        self._wait_v2()
         self._v2_access_log_reader = None
         self._v2_error_log_reader = None
         signal.signal(signal.SIGCHLD, prev)
 
+    def _create_logger(self, level=logging.INFO):
+        self._logger = logging.getLogger('Firev2-Monitor')
+        self._logger.setLevel(level)
+        handler = logging.FileHandler(self.get_log_filename())
+        handler.setFormatter(logging.Formatter(
+            '[%(asctime)s - PID %(process)d - %(levelname)s]: %(message)s'))
+        self._logger_fd = handler.stream.fileno()
+        self._logger.addHandler(handler)
+
     def _daemonize(self):
-        print('daemonizing')
+        self._logger.info('daemonizing')
         import sys
         if os.fork() != 0:
             sys.exit()
@@ -190,30 +184,30 @@ class Monitor(Process):
         os.chdir('/')
         # TODO
         fd_max = os.sysconf(os.sysconf_names['SC_OPEN_MAX'])
-        print(fd_max)
+        # print(fd_max)
         fd_max = 1024
-        for fd in range(3, fd_max):
+        for fd in range(0, fd_max):
             close = True
-            if fd == self._sync_pipe[1]:
+            if fd == self._logger_fd:
                 close = False
-
-            for fp in self._parent_log_pipe[1]:
-                if fp and fd == fp.fileno():
-                    close = False
-            if not close:
-                continue
-            try:
-                os.close(fd)
-            except OSError:
-                pass
-        os.close(0)
-        sys.stdin = open('/dev/null')
+            if close:
+                try:
+                    os.close(fd)
+                except OSError:
+                    pass
+        fp = open('/dev/null')
+        for fd in range(0, 3):
+            os.dup2(fp.fileno(), fd)
+        sys.stdin = sys.stdout = sys.stderr = fp
 
     def _write_pid_file(self):
+        self._logger.debug('writing pid file')
         with open(self.get_pid_filename(), 'w') as fp:
             fp.write(f'{os.getpid()}\n')
+        self._logger.debug('pid file written')
 
     def _listen_log_sockets(self):
+        self._logger.debug('creating unix domain sockets')
         if os.path.exists(self._sock_access_fname):
             os.unlink(self._sock_access_fname)
         if os.path.exists(self._sock_error_fname):
@@ -224,11 +218,9 @@ class Monitor(Process):
         self._sock_error.bind(self._sock_error_fname)
         self._sock_access.listen(100)
         self._sock_error.listen(100)
-        print('unix daemon sockets created')
+        self._logger.debug('unix domain sockets created')
 
     def _install_signal_handler(self):
-
-        # step 3: install handlers for SIGHUP and SIGTERM
         def handler(signum, frame):
             if signum == signal.SIGCHLD:
                 self._child_exited = True
@@ -239,11 +231,13 @@ class Monitor(Process):
                 self._should_restart = True
                 raise InterruptedError
 
+        self._logger.debug('installing signal handlers')
         signal.signal(signal.SIGTERM, handler)
         signal.signal(signal.SIGCHLD, handler)
         signal.signal(signal.SIGHUP, handler)
         signal.siginterrupt(signal.SIGTERM, True)
         signal.siginterrupt(signal.SIGHUP, True)
+        self._logger.debug('signal handlers installed')
 
     def _open_v2_log_files(self):
         config = self._config
@@ -264,6 +258,7 @@ class Monitor(Process):
         self._config = config
 
     def _start_v2ray(self):
+        self._logger.info('starting v2ray')
         self._v2_config_filename = self._get_work_dir() + '/v2ray_config.json'
         with open(self._v2_config_filename, 'w') as fp:
             json.dump(self._config, fp, indent=4, ensure_ascii=False)
@@ -273,28 +268,28 @@ class Monitor(Process):
                                     close_fds=True)
         self._v2_access_log_reader = self._v2.stdout
         self._v2_error_log_reader = self._v2.stderr
-        print('v2ray started')
+        self._logger.info('v2ray started')
 
     def _poll(self):
         while True:
             rfd = [self._v2_access_log_reader, self._v2_error_log_reader,
                    self._sock_access, self._sock_error]
-            wfd = [f for f, d in self._data_to_write.items() if d]
+            wfd = [f for f, d in self._redirect_data_to_write.items() if d]
             try:
                 rfd, wfd, xfd = select.select(rfd, wfd, [])
             except InterruptedError:
                 if self._should_exit:
-                    print('SIGTERM received. exiting...')
+                    self._logger.warning('SIGTERM received. exiting...')
                     signal.signal(signal.SIGCHLD, signal.SIG_BLOCK)
                     self._v2.kill()
                     self._wait_v2()
                     sys.exit(-1)
                 elif self._should_restart:
-                    print('SIGHUP received. restarting...')
+                    self._logger.warning('SIGHUP received. restarting...')
                     self._should_restart = False
                     return
             if self._child_exited:
-                print('v2ray terminated unexpectedly. exiting...')
+                self._logger.error('v2ray terminated unexpectedly. exiting...')
                 self._wait_v2()
                 sys.exit(-1)
             self._dispatch_fd(rfd, wfd)
@@ -306,6 +301,7 @@ class Monitor(Process):
         self._flush_all_redirect_log()
 
     def _dispatch_fd(self, rfds: List[IO], wfds: List[IO]):
+        self._logger.info('start to redirect logs')
         for fd in rfds:
             if fd == self._v2_access_log_reader:
                 data = self._v2_access_log_reader.read1()
@@ -325,20 +321,22 @@ class Monitor(Process):
                 self._add_redirect_file(error_file=fp)
 
         for fd in wfds:
-            if fd in self._data_to_write:
+            if fd in self._redirect_data_to_write:
                 self._flush_redirect_log(fd)
 
     def _add_redirect_file(self, access_file=None, error_file=None):
         if access_file:
             self._redirect_access_files.append(access_file)
-            self._data_to_write[access_file] = b''
+            self._redirect_data_to_write[access_file] = b''
+            self._logger.info('new access log redirection added')
         if error_file:
             self._redirect_error_files.append(error_file)
-            self._data_to_write[error_file] = b''
+            self._redirect_data_to_write[error_file] = b''
+            self._logger.info('new error log redirection added')
 
     def _remove_redirect_file(self, file):
-        self._data_to_write[file] = b''
-        self._data_to_write.pop(file)
+        self._redirect_data_to_write[file] = b''
+        self._redirect_data_to_write.pop(file)
         if file in self._redirect_access_files:
             self._redirect_access_files.remove(file)
         else:
@@ -347,17 +345,17 @@ class Monitor(Process):
     def _add_redirect_data(self, access_data=None, error_data=None):
         if access_data:
             for f in self._redirect_access_files:
-                self._data_to_write[f] += access_data
+                self._redirect_data_to_write[f] += access_data
         if error_data:
             for f in self._redirect_error_files:
-                self._data_to_write[f] += error_data
+                self._redirect_data_to_write[f] += error_data
 
     def _flush_all_redirect_log(self):
-        for fd in list(self._data_to_write.keys()):
+        for fd in list(self._redirect_data_to_write.keys()):
             self._flush_redirect_log(fd)
 
     def _flush_redirect_log(self, fd):
-        to_write = self._data_to_write[fd]
+        to_write = self._redirect_data_to_write[fd]
         while to_write:
             try:
                 written_len = fd.write(to_write)
@@ -369,32 +367,7 @@ class Monitor(Process):
                 self._remove_redirect_file(fd)
                 break
             to_write = to_write[written_len:]
-            self._data_to_write[fd] = to_write
-
-    def _logfile_as_stdout_err(self):
-        f = open(f'{self.get_log_filename()}', 'a')
-        os.close(1)
-        os.dup2(f.fileno(), 1)
-        os.close(2)
-        os.dup2(f.fileno(), 2)
-
-    def create_v2_log_pipe(self) -> Tuple[FileIO, FileIO]:
-        if any(self._parent_log_pipe[0]):
-            assert all(self._parent_log_pipe[0])
-            return self._parent_log_pipe[0]
-        else:
-            assert not self.is_alive(), 'v2ray already started'
-            acc_fd = os.pipe()
-            err_fd = os.pipe()
-            fcntl.fcntl(acc_fd[1], fcntl.F_SETFL, os.O_NONBLOCK)
-            fcntl.fcntl(err_fd[1], fcntl.F_SETFL, os.O_NONBLOCK)
-            acc_read = io.FileIO(acc_fd[0], mode='r', closefd=True)
-            acc_write = io.FileIO(acc_fd[1], mode='wb', closefd=True)
-            err_read = io.FileIO(err_fd[0], mode='r', closefd=True)
-            err_write = io.FileIO(err_fd[1], mode='wb', closefd=True)
-            self._add_redirect_file(access_file=acc_write, error_file=err_write)
-            self._parent_log_pipe = ((acc_read, err_read), (acc_write, err_write))
-            return self._parent_log_pipe[0]
+            self._redirect_data_to_write[fd] = to_write
 
     @classmethod
     def _get_work_dir(cls):
